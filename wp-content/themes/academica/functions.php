0b3a8b03802d10eeca614a32c24133004f074373
Jason Kim <jason.kim.jiho@gmail.com>
2015-05-21 07:27:06 -0400

    title fixed

--- wp-content/themes/academica/functions.php
+++ wp-content/themes/academica/functions.php
@@ -183,7 +183,7 @@ function academica_wp_title( $title, $sep ) {
 	global $paged, $page;
 
 	// Add the site name.
-	$title .= get_bloginfo( 'name' );
+	// $title .= get_bloginfo( 'name' );
 
 	// Add the site description for the home/front page.
 	$site_description = get_bloginfo( 'description', 'display' );
